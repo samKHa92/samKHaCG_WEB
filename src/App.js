52701e217a49176fee3f09e2e5f144addfc22320
samKHa92 <samkha@gfmstudio.com>
2022-10-11 22:08:35 +0400

    Configured error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {HomePage} from "./pages/HomePage";
 import {NavigationBar} from "./components/NavigationBar";
 import {Footer} from "./components/Footer";
+import {ErrorPage} from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <NavigationBar/>
           <Routes>
             <Route path="/" element={<HomePage/>}/>
+              <Route path="*" element={<ErrorPage/>}/>
           </Routes>
           <Footer/>
         </div>
